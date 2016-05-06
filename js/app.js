5b3029f113e1d7c6f2042f48fefc32e0a3c7f73a
adai183 <andreas.daiminger@gmail.com>
2016-05-06 19:34:11 +0200

    add research view

--- js/app.js
+++ js/app.js
@@ -1,3 +1,17 @@
 $(document).ready(function($){
-    console.log("let's go!");
+
+    // Bootstrap Scrollspy
+    var $body   = $(document.body);
+    var navHeight = $('.navbar').outerHeight(true) + 10;
+
+    $('#sidebar').affix({
+        offset: {
+            top: 245
+        }
+    });
+
+    $body.scrollspy({
+	        target: '#leftCol',
+	        offset: navHeight
+    });
 });
